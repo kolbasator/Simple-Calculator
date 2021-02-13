e788240a023d73160e82965517e88050239a8cb5
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-02-13 21:08:54 +0600

    calculator

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Text;
+using System.Text; 
 
 namespace Calculator
 {
     public class SimpleCalculator
     {
         static int Priority(string symbol)
-        { 
-            if (symbol == "*" || symbol == "/")
+        {
+            if(symbol=="sin" || symbol == "cos" || symbol == "ctn" || symbol == "tan" || symbol=="log")
+            {
+                return 5;
+            }
+            else if (symbol == "^")
+            {
+                return 4;
+            }
+            else if (symbol == "*" || symbol == "/")
             {
                 return 3;
             }
@@ -25,31 +33,44 @@ namespace Calculator
             {
                 return -1;
             }
-        } 
+        }
+    
         public List<string> ShuntingYard(string expression)
         {
-            string[] tokens = expression.Split(null);
+            string[] tokens = expression.Split(null); 
             List<string> output = new List<string>();
-            Stack<char> operators = new Stack<char>();
+            Stack<string> operators = new Stack<string>();
             //Рассматриваем поочередно каждый символ:
             for (int i = 0; i < tokens.Length; i++)
             {
                 //1.Если этот символ -число(или переменная), то просто помещаем его в выходную строку  
-                if (IsNumber(tokens[i]))
+                if (tokens[i] == "PI")
+                {
+                    output.Add("3.14");
+                    continue;
+                }
+                if (IsNumber(tokens[i]) || tokens[i]==".")
                 {
                     output.Add(tokens[i]);
                     continue;
                 }
+              
                 switch (tokens[i])
-                {
-                    case "(": 
+                { 
+                    case "(":
                         //3.Если текущий символ -открывающая скобка, то помещаем ее в стек.
-                        operators.Push(Convert.ToChar(tokens[i]));
+                        operators.Push(tokens[i]);
                         break;
+                    case "^":
                     case "*":
                     case "/":
                     case "+":
                     case "-":
+                    case "sin":
+                    case "tan":
+                    case "ctn":
+                    case "cos":
+                    case "log":
                         #region Comments
                         //2.Если символ - знак операции(+, -, *, / ), то проверяем приоритет данной операции.
                         //Операции умножения и деления имеют наивысший приоритет(допустим он равен 
@@ -67,14 +88,14 @@ namespace Calculator
                         {
                             output.Add(operators.Pop().ToString());
                         }
-                        operators.Push(Convert.ToChar(tokens[i]));
+                        operators.Push(tokens[i]);
                         break;
                     case ")":
                         #region Comments
                         //4.Если текущий символ -закрывающая скобка, то извлекаем символы из стека в выходную строку до тех пор, пока не встретим в стеке
                         //открывающую скобку(т.е.символ с приоритетом, равным 1), которую следует просто уничтожить. Закрывающая скобка также уничтожается.
                         #endregion
-                        while (operators.Peek() != '(')
+                        while (operators.Peek() != "(")
                         {
                             output.Add(operators.Pop().ToString());
                         }
@@ -104,7 +125,8 @@ namespace Calculator
             //Алгоритм высчитывания результата
             Stack<double> resultStack = new Stack<double>();
             foreach (var token in tokens)
-            { 
+            {
+                double leftOperand;
                 double rightOperand;
                 //Если символ в переработанном выражении число ,то помещаем его в стек.
                 if (IsNumber(token.ToString()))
@@ -126,19 +148,48 @@ namespace Calculator
                         break;
                     case "/":
                         rightOperand = resultStack.Pop();
-                        double leftOperand = resultStack.Pop();
-                        if (Math.Round(rightOperand) == 0 || Math.Round(leftOperand) == 0)
+                        leftOperand = resultStack.Pop();
+                        if (Math.Round(rightOperand) == 0 )
                         {
                             throw new InvalidOperationException("Не делай так");
                         }
                         resultStack.Push(leftOperand / rightOperand);
+                        break;
+                    case "^":
+                        rightOperand = resultStack.Pop();
+                        leftOperand = resultStack.Pop();
+                        resultStack.Push(Math.Pow(leftOperand, rightOperand));
+                        break;
+                    case "sin":
+                        double numberToSin = resultStack.Pop();
+                        resultStack.Push(Math.Sin(numberToSin));
+                        break;
+                    case "cos":
+                        double numberToCos = resultStack.Pop();
+                        resultStack.Push(Math.Cos(numberToCos));
+                        break;
+                    case "ctn":
+                        double numberToCtn = resultStack.Pop();
+                        resultStack.Push(1/Math.Tan(numberToCtn));
+                        break;
+                    case "tan":
+                        double numberToTan = resultStack.Pop();
+                        resultStack.Push(Math.Tan(numberToTan));
+                        break;  
+                    case "log":
+                        double numberToLog = resultStack.Pop();
+                        resultStack.Push(Math.Log(numberToLog));
+                        break;
+                    case "ex":
+                        double numberToEx = resultStack.Pop();
+                        resultStack.Push(Math.Exp(numberToEx));
                         break;
                 }
             }
             if (resultStack.Count != 1)
             {
                 throw new InvalidOperationException();
-            } 
+            }
             return resultStack.Pop();
         }
         public double Calculate(string expression)
--- Calculator/CalculatorTests/UnitTest1.cs
+++ Calculator/CalculatorTests/UnitTest1.cs
@@ -7,6 +7,7 @@ namespace CalculatorTests
 {
     public class Tests
     {
+        public SimpleCalculator calculator = new SimpleCalculator(); 
         [SetUp]
         public void Setup()
         {
@@ -14,19 +15,14 @@ namespace CalculatorTests
 
         [Test]
         public void CalculatorNullTest()
-        {
-            SimpleCalculator calculator = new SimpleCalculator();
+        { 
             Action act = () => calculator.Calculate("2 / 0");
             act.Should().Throw<InvalidOperationException>()
-                .WithMessage("Не делай так");
-            Action act2 = () => calculator.Calculate("0 / 2");
-            act2.Should().Throw<InvalidOperationException>()
                 .WithMessage("Не делай так");
         }
         [Test]
         public void InvalidInputTest()
-        {
-            SimpleCalculator calculator = new SimpleCalculator();
+        { 
             Action act = () => calculator.Calculate("A + B");
             act.Should().Throw<InvalidOperationException>()
                 .WithMessage("Не стоит пихать что попало в калькулятор ! Будь разумным.");
@@ -34,28 +30,70 @@ namespace CalculatorTests
 
         [Test]
         public void CalculatorTest1()
-        {
-            SimpleCalculator calculator = new SimpleCalculator();
-            Assert.AreEqual(8, calculator.Calculate("2 * ( 2 + 2 )"));
+        { 
+            calculator.Calculate("2 * ( 2 + 2 )").Should().Be(8);
         }
         [Test]
         public void CalculatorTest2()
         {
-            SimpleCalculator calculator = new SimpleCalculator();
-            Assert.AreEqual(9442509, calculator.Calculate("418560 / ( 34 * 25 - 196 ) * 708 - 500347 / 983 + 8989898"));
+            calculator.Calculate("418560 / ( 34 * 25 - 196 ) * 708 - 500347 / 983 + 8989898").Should().Be(9442509);
         }
         [Test]
         public void CalculatorTest3()
-        {
-            SimpleCalculator calculator = new SimpleCalculator();
-            Assert.AreEqual(0.2211802765907797, calculator.Calculate("20 * 20 / ( 10 - ( 4.777 * 7.666 + 6 ) + 5 * ( 43.32 * 8.5 ) )"));
+        { 
+            calculator.Calculate("20 * 20 / ( 10 - ( 4.777 * 7.666 + 6 ) + 5 * ( 43.32 * 8.5 ) )").Should().Be(0.2211802765907797);
         }
         [Test]
         public void CalculatorTest4()
-        {
-            SimpleCalculator calculator = new SimpleCalculator();
-            Assert.AreEqual(60263.1557377049180328, calculator.Calculate("( 1338 + 58487 ) + 123 / 244 * 38 + ( 1028 - 609 )"));
+        { 
+            calculator.Calculate("( 1338 + 58487 ) + 123 / 244 * 38 + ( 1028 - 609 )").Should().Be(60263.1557377049180328);
         }
-        
+        [Test]
+        public void CalculatorTest5()
+        { 
+            calculator.Calculate("2 * 7 ^ 2").Should().Be(98);
+        }
+        [Test]
+        public void CalculatorTest6()
+        { 
+            calculator.Calculate("2.5 * ( 1 + ( 2 ^ 0 ) )").Should().Be(5);
+        }
+        [Test]
+        public void CalculatorTest7()
+        { 
+            calculator.Calculate("2 / 2 * ( 100 + 40 ) / ( 2 ^ 2 + 4 )").Should().Be(17.5);
+        }
+        [Test]
+        public void CalculatorTest8()
+        {
+            calculator.Calculate("1 + cos ( 1 / 2 ) + sin ( 1 / 2 )").Should().Be(2.3570081004945758);
+        }
+        [Test]
+        public void CalculatorTest9()
+        {
+            calculator.Calculate("sin ( 5 ) * PI").Should().Be(-3.011022222442255);
+        }
+        [Test]
+        public void CalculatorTest10()
+        {
+            calculator.Calculate("ctn ( 5 ) + 5").Should().Be(4.704187084467255); 
+        }
+        [Test]
+        public void CalculatorTest11()
+        {
+            calculator.Calculate("tan ( 5 ) + 5").Should().Be(1.6194849937534141); 
+        }
+        public void CalculatorTest12()
+        {
+            calculator.Calculate("ex ( 6 ) * 6").Should().Be(2420.57276096);
+        }
+        [Test]
+        public void CalculatorTest13()
+        {
+            calculator.Calculate("log ( 5 ) + 10").Should().Be(11.6094379124341);
+        }
+
+
+
     }
 }
